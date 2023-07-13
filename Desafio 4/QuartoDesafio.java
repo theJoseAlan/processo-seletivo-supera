7926008d781bba2f4f8b44155f8b3a5d5354c2d2
theJoseAlan <salesalan854@gmail.com>
2023-07-13 09:03:50 -0300

    Resolução do Desafio 4

--- Desafio 4/QuartoDesafio.java
+++ Desafio 4/QuartoDesafio.java
@@ -1,5 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class QuartoDesafio {
+
     public static void main(String[] args) {
 
+
+        Scanner input = new Scanner(System.in);
+
+        List<String> palavras = new ArrayList<>();
+
+        int qtdeExecuções = input.nextInt();
+        input.nextLine();
+
+        for (int i = 0; i < qtdeExecuções; i++) {
+            String texto = input.nextLine();
+
+            int length = texto.length();
+            StringBuilder textoCorrigido = new StringBuilder(length);
+
+
+            for (int k = length / 2 - 1; k >= 0; k--) {
+                textoCorrigido.append(texto.charAt(k));
+            }
+
+            
+            for (int k = length - 1; k >= length / 2; k--) {
+                textoCorrigido.append(texto.charAt(k));
+            }
+
+            palavras.add(textoCorrigido.toString());
+        }
+
+        for (String palavra : palavras) {
+            System.out.println(palavra);
+        }
     }
 }
